be2aedbc9587353dacaa2d80a996f4190a0839c3
savatge <joaquinsavatge@gmail.com>
2026-06-09 20:09:02 -0300

    Se hace clse aauto

---
+++ SistemaVehiculo/entities/Auto.cs
@@ -0,0 +1,17 @@
+public class Auto : Vehiculo
+{
+    public Auto(string marca, string modelo, int año)
+        : base(marca, modelo, año)
+    { 
+    }
+
+    public override void Acelerar()
+    {
+        Console.WriteLine("El auto está acelerando.");
+    }
+
+    public override void Frenar()
+    {
+        Console.WriteLine("El auto está frenando.");
+    }
+}
